c63dd07f9f0bb994a8d46d824e3931e59d3ca6b7
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2015-02-26 20:54:05 +1100

    Initial Commit
    Hello World

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jack Watson-Hamblin on 26/02/2015.
+//  Copyright (c) 2015 ACME Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindFromSegue(segue: UIStoryboardSegue) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
